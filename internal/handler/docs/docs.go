66d34675655c09727de121013db4a54f6eb249d1
the-sabra <omarsabra509@gmail.com>
2024-11-12 22:06:09 +0200

    fix : can you try the endpoint in swagger docs

--- internal/handler/docs/docs.go
+++ internal/handler/docs/docs.go
@@ -675,7 +675,7 @@ const docTemplate = `{
                 "created_at": {
                     "type": "string"
                 },
-                "date_of_birth": {
+                "dob": {
                     "type": "string"
                 },
                 "email": {
@@ -704,7 +704,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "O-Clinic API",
